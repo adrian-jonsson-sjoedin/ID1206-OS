648e9a0027d981852e11ddfea490cf219b3e778e
Peter Bernath <nbernath@gmail.com>
2023-11-09 12:47:00 +0100

    updated part_1.c

--- labb_1/part_1.c
+++ labb_1/part_1.c
@@ -5,11 +5,11 @@
 #include <sys/wait.h>
 
 int main(void) {
-    int pipefds[2];
+    int fd[2];
     pid_t pid;
 
     // Create a pipe
-    if (pipe(pipefds) == -1) {
+    if (pipe(fd) == -1) {
         perror("pipe creation failed");
         exit(EXIT_FAILURE);
     }
@@ -25,16 +25,16 @@ int main(void) {
     if (pid == 0) {  
         // Child process
         // Close the read end of the pipe, not needed in the child
-        close(pipefds[0]);
+        close(fd[0]);
 
         // Redirect stdout to the write end of the pipe
-        if (dup2(pipefds[1], STDOUT_FILENO) == -1) {
+        if (dup2(fd[1], STDOUT_FILENO) == -1) {
             perror("dup2 redirect failed in child");
             exit(EXIT_FAILURE);
         }
 
         // Close the original write end of the pipe
-        close(pipefds[1]);
+        close(fd[1]);
 
         // Replace the child process with the "ls /" command
         execlp("ls", "ls", "/", NULL);
@@ -47,19 +47,19 @@ int main(void) {
         int status;
 
         // Close the write end of the pipe, not needed in the parent
-        close(pipefds[1]);
+        close(fd[1]);
 
         // Wait for the child process to finish
         waitpid(pid, &status, 0);
 
         // Redirect stdin to the read end of the pipe
-        if (dup2(pipefds[0], STDIN_FILENO) == -1) {
+        if (dup2(fd[0], STDIN_FILENO) == -1) {
             perror("dup2 redirect failed in parent");
             exit(EXIT_FAILURE);
         }
 
         // Close the original read end of the pipe
-        close(pipefds[0]);
+        close(fd[0]);
 
         // Replace the parent process with the "wc -l" command
         execlp("wc", "wc", "-l", NULL);
